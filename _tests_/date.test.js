f889d5a27d942e71cb69581230f932fcdd42c0be
Student <student@epicodus-2.hsd1.or.comcast.net>
2022-11-09 14:10:22 -0800

    add test for return the day of the week for the given date, and test passed
    co-authored-by: Bodie Wood <bodiewood@hotmail.com>

--- _tests_/date.test.js
+++ _tests_/date.test.js
@@ -6,7 +6,35 @@ describe("CalcDate", () => {
 		const aDate = new CalcDate(1, 24, 1990);
 		expect(aDate.month).toEqual(1);
 		expect(aDate.day).toEqual(24);
-		expect(aDate.year).toEqual(1990)
+		expect(aDate.year).toEqual(1990);
 	});
+
+	// test('should correctly determine if date is not valid', () => {
+	// 	const aDate = new CalcDate(-1, "twenty", "#");
+	// 	expect(aDate.checkDate()).toEqual("not a date");
+	// });
+
+	// test('should correctly determine if a date is valid', () => {
+	// 	const aDate = new CalcDate(1,24,1990);
+	// 	expect(aDate.checkDate()).toEqual("you got this");
+	// });
+
+	// test('should check if year is greater than 1800', () => {
+	// 	const aDate = new CalcDate(1,24,1990);
+	// 	expect(aDate.checkDate()).toEqual('greater than 1800');
+	// });
+
+		// test('should return remainder of day of week calculation', () => {
+		// 	const aDate = new CalcDate(5,24,1990);
+		// 	expect(aDate.checkDate()).toEqual(5);
+		// });
+
+		test('should return the day of week for the given day', () => {
+			const aDate = new CalcDate(1,24,1990);
+			expect(aDate.checkDate()).toEqual('Wednesday');
+		});
+
+
+	
 });
 
--- src/date.js
+++ src/date.js
@@ -2,9 +2,118 @@ export default class CalcDate {
   constructor(month, day, year) {
     this.month = month;
     this.day = day;
-    this.year = year
+    this.year = year;
   }
-  checkDay() {
+
+  checkDate() {
+    // const jan = 1;
+    // const feb = 4;
+    // const march = 4;
+    // const april = 0;
+    // const may = 2;
+    // const june = 5;
+    // const july = 0;
+    // const aug = 3;
+    // const sept = 6;
+    // const oct = 1;
+    // const nov = 4;
+    // const dec = 6;
+    // const janLeapYear = 0;
+    // const febLeapYear = 3;
+
+    let monthKey = 0;
+
+
+
+    switch (this.month) {
+      case 1:
+        monthKey = 1;
+        break;
+      case 2:
+        monthKey = 4;
+        break;
+      case 3:
+        monthKey = 4;
+        break;
+      case 4:
+        monthKey = 0;
+        break;
+      case 5:
+        monthKey = 2;
+        break;
+      case 6:
+        monthKey = 5;
+        break;
+      case 7:
+        monthKey = 0;
+        break;
+      case 8:
+        monthKey = 3;
+        break;
+      case 9:
+        monthKey = 6;
+        break;
+      case 10:
+        monthKey = 4;
+        break;
+      case 11:
+        monthKey = 1;
+        break;
+      case 12:
+        monthKey = 6;
+        break;
+      default:
+        console.log("Enter a valid number");
+    }
+
+    const newArr = [];
+    let yearArr = Array.from(String(this.year), Number);
+    yearArr.forEach(function (element, index) {
+      if (index > 1) {
+        newArr.push(element);
+      }
+    });
+
+    let yearNumString = newArr.join('');
+    let finalYearNum = parseInt(yearNumString);
+    const num1 = Math.trunc(finalYearNum * .25);
+    const num2 = finalYearNum + num1 + this.day + monthKey;
+    let num3 = 0;
+
+    if (this.year > 1800) {
+      num3 = ((num2) % 7);
+
+    }
+    let weekDay = '';
+    switch (num3) {
+      case 1:
+        weekDay = 'Sunday';
+        break;
+      case 2:
+        weekDay = 'Monday';
+        break;
+      case 3:
+        weekDay = 'Tuesday';
+        break;
+      case 4:
+        weekDay = 'Wednesday';
+        break;
+      case 5:
+        weekDay = 'Thursday';
+        break;
+      case 6:
+        weekDay = 'Friday';
+        break;
+      case 7:
+        weekDay = 'Saturday';
+        break;
+      default:
+        console.log('Not a day of week');
+    }
+
+    return weekDay;
 
   }
+
+
 }
